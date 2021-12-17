0ec409a73268a05e6bc6ccf36c80a6e9f4c3d2fd
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-17 11:26:55 -0500

    [CREATE]

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
