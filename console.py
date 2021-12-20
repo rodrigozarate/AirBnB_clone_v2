87c5e17708554a56019f7a6ba7e0090a774a9cf1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-20 09:47:38 -0500

    [FIX] is changed to ==

--- console.py
+++ console.py
@@ -74,8 +74,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] == '}'\
+                            and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -135,7 +135,6 @@ class HBNBCommand(cmd.Cmd):
                 new_dict[key] = value
         return new_dict
 
-
     def do_create(self, args):
         """ Create an object of any class"""
         if len(args) == 0:
@@ -165,6 +164,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, key, value)
             new_instance.save()
             print(new_instance.id)
+
         except NameError:
             print("** class doesn't exist **")
             return
@@ -323,10 +323,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
