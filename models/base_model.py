b002e27d477c1c57410a63b79b8c17d1fb0ac4b4
John Doe <juanestebancope@outlook.com>
2021-12-20 18:17:24 -0500

    add storage.new(self)

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-
+    created_at = Column(DateTime(60), nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime(60), nullable=False, default=created_at)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
@@ -35,6 +36,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
