c6985632c1dccc92fdbada4d84b2f753c42ad0de
John Doe <juanestebancope@outlook.com>
2021-12-22 16:11:39 -0500

    import getenv

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
