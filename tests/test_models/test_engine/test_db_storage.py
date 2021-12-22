c8b942b50c8e397b129f075da27583401447e318
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-22 14:23:47 -0500

    [FIX]

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module for testing DBstorage"""
+""" Test DBstorage"""
 import unittest
 from models.base_model import BaseModel
 from models import storage
@@ -24,3 +24,24 @@ class test_dbstorage(unittest.TestCase):
     def test_obj_list_empty(self):
         """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
+
+    def test_reload_from_nonexistent(self):
+        """ Nothing happens if file does not exist """
+        self.assertEqual(storage.reload(), None)
+
+    def test_type_objects(self):
+        """ Confirm __objects is a dict """
+        self.assertEqual(type(storage.all()), dict)
+
+    def test_store(self):
+        """ Test if an object is store in the database """
+        new = State(name="Florida")
+        new.save()
+        _id = new.to_dict()['id']
+        self.assertIn(new.__class__.__name__ + '.' + _id,
+                        storage.all(type(new)).keys())
+
+    def test_storage_var_created(self):
+        """ FileStorage object storage created """
+        from models.engine.db_storage import DBStorage
+        self.assertEqual(type(storage), DBStorage)
