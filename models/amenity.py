20b6137d3b33b798260ab98e7bd563e6d244ede1
John Doe <juanestebancope@outlook.com>
2021-12-22 16:11:10 -0500

    different association

--- models/amenity.py
+++ models/amenity.py
@@ -8,8 +8,12 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class """
     __tablename__ = "amenities"
-
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+
+        from models.place import place_amenity
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        place_amenities = ''
