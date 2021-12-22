150c9cce1ef451cb48041f8d6fb0f3ad98269162
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-21 23:12:34 -0500

    [UPDATE]

---
+++ tests/test_console/test_console.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+""" Test console.py """
+import unittest
+import pep8
+
+
+class tests_console(unittest.TestCase):
+    """ Test Console """
+
+    def test_pep8_conformance(self):
+        """ pep8 """
+        stylemodel = pep8.StyleGuide(quiet=True)
+        result = styleModel.check_files(['console.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
