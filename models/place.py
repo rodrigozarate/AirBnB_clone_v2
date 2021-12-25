661c66d37c6c7dc092b887dd3020304e55537147
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-24 23:22:50 -0500

    [FIX]

--- models/place.py
+++ models/place.py
@@ -6,7 +6,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy import *
-# from models.amenity import Amenity
 from models.review import Review
 from os import getenv
 
