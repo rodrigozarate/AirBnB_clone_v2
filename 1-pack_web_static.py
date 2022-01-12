dafec025b7108f3de30cef1b9ad078c3f2d53d27
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-12 11:07:34 -0500

    [CREATE]

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Paking using Fabric """
+
+import datetime
+from fabric.api import local
+import os.path
+
+
+def do_pack():
+    """ tgz files and give a time based name """
+    the_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    the_pack = "versions/web_static_{}.tgz".format(the_date)
+    if os.path.isdir("versions") is false:
+        local(" mkdir versions")
+    local('tar -cvzf ' + the_pack + ' web_static')
+    if os.path.exists(the_pack):
+        return the_pack
+    return None
