894531973e3c4980c7de90ef84a7908c1a01385a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-14 08:37:01 -0500

    [FIX]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,15 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y nginx
+sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
 sudo chown ubuntu:ubuntu -hR /data/
 sudo echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown ubuntu:ubuntu -hR /data/
 sudo sed -i '60i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
