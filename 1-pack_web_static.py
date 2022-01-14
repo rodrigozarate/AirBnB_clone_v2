8d4f111a815584c34315def3b2a869618fac7a7e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-13 23:17:16 -0500

    [FIX]

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     """ tgz files and give a time based name """
     the_date = datetime.now().strftime("%Y%m%d%H%M%S")
     the_pack = "versions/web_static_{}.tgz".format(the_date)
-    if os.path.isdir("versions") is false:
+    if os.path.isdir("versions") is False:
         local(" mkdir versions")
     local('tar -cvzf ' + the_pack + ' web_static')
     if os.path.exists(the_pack):
