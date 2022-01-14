2c0c586c975707a8366b8740f2a34284068fe5f7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-14 08:49:16 -0500

    [FIX]

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-""" Paking using Fabric """
+""" Packing using Fabric """
 
 import datetime
 from fabric.api import local
-import os.path
 
 
 def do_pack():
     """ tgz files and give a time based name """
     the_date = datetime.now().strftime("%Y%m%d%H%M%S")
     the_pack = "versions/web_static_{}.tgz".format(the_date)
-    if os.path.isdir("versions") is False:
-        local("mkdir -p versions")
-    local('tar -cvzf ' + the_pack + ' web_static')
-    if os.path.exists(the_pack):
-        return the_pack
-    return None
+    local("mkdir -p versions")
+    compressed = local("tar -cvzf {} web_static".format(the_pack))
+    if compressed.failed:
+        return None
+    return the_pack
