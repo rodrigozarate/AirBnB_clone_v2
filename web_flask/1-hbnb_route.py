65f08ef6c8fb8f4286c0ff9fad8e9bc69726011a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-29 20:51:40 -0500

    [CREATE]

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Flask route model """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ / route  """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """ /hbnb route """
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
