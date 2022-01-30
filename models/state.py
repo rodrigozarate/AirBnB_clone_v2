e5eb8b1e9f533f3eb8507db8fe43481fd696fd37
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-30 12:43:32 -0500

    [UPDATE]

--- models/state.py
+++ models/state.py
@@ -3,23 +3,26 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+import os
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='states')
-
-    @property
-    def cities(self):
-        """ getter attribute cities that returns the list of City
-        instances with state_id equals to the current State.id"""
-        from models import storage
-        from models.city import City
-        list = []
-        cities = storage.all(City)
-        for value in cities.values():
-            if value.state_id == self.id:
-                list.append(value)
-        return list
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='states')
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute cities that returns
+                the list of City instances with state_id
+                equals to the current State.id
+            """
+            from models import storage
+            from models.city import City
+            list = []
+            cities = storage.all(City)
+            for value in cities.values():
+                if value.state_id == self.id:
+                    list.append(value)
+            return list
