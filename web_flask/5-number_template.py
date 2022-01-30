e82d9003d5bf3ec5cfa635cbe2c4970ebbd335e7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-29 22:00:35 -0500

    [FIX]

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbertemplate(n):
+def number_template(n):
     """ /number_template path """
     return render_template('5-number.html', n=n)
 
